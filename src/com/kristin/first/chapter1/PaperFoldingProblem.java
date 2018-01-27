7d53ee8d747f7d556261422da954317e58405629
Lee0526 <3215758767@qq.com>
2018-01-27 17:58:30 +0800

    2018.01.27

---
+++ src/com/kristin/first/chapter1/PaperFoldingProblem.java
@@ -0,0 +1,31 @@
+package com.kristin.first.chapter1;
+
+/**
+ * @author 李航
+ * @school 哈尔滨理工大学
+ * @date 2018/1/27 16:36
+ * @desc 1.折纸问题:输出结果是二叉树的中序遍历
+ **/
+public class PaperFoldingProblem {
+    public static void printAllfolds(int foldTimes) {
+        if (foldTimes < 1) {
+            return;
+        }
+        printporcess(1, foldTimes, true);
+        System.out.println();
+    }
+
+    private static void printporcess(int index, int foldtimes, boolean isDown) {
+        if (index == foldtimes) {
+            System.out.print(isDown ? "Down " : "Up ");
+            return;
+        }
+        printporcess(index + 1, foldtimes, true);
+        System.out.print(isDown ? "Down " : "Up ");
+        printporcess(index + 1, foldtimes, false);
+    }
+
+    public static void main(String[] args) {
+        printAllfolds(4);
+    }
+}
---
+++ src/com/kristin/first/chapter1/PrintMatrixCircle.java
@@ -0,0 +1,67 @@
+package com.kristin.first.chapter1;
+
+/**
+ * @author 李航
+ * @school 哈尔滨理工大学
+ * @date 2018/1/27 16:54
+ * @desc 转圈打印矩阵
+ **/
+public class PrintMatrixCircle {
+    public static void printMatrixCircle(int[][] matrix) {
+        if (matrix == null || matrix[0] == null) {
+            return;
+        }
+        int upR = 0;
+        int upC = 0;
+        int downR = matrix.length - 1;
+        int downC = matrix[0].length - 1;
+        while (upC <= downC && upR <= downR) {
+//            if (upC == downC) {
+//                for (int i = upR; i <= downR; i++) {
+//                    System.out.print(matrix[i][upC] + " ");
+//                }
+//            } else if (upR == downR) {
+//                for (int i = upC; i <= downC; i++) {
+//                    System.out.print(matrix[upR][upC] + " ");
+//                }
+//            } else {
+//                printCircle(matrix, upR, upC, downR, downC);
+//            }
+            /*我觉得不用加上面的代码,但是左神写了,应该是我哪个细节没考虑到吧,我再研究*/
+            printCircle(matrix, upR, upC, downR, downC);
+            upC++;
+            upR++;
+            downC--;
+            downR--;
+        }
+    }
+
+    private static void printCircle(int[][] matrix, int upR, int upC, int downR, int downC) {
+        int col = upC;
+        while (col != downC) {
+            System.out.print(matrix[upR][col] + " ");
+            col++;
+        }
+        int row = upR;
+        while (row != downR) {
+            System.out.print(matrix[row][col] + " ");
+            row++;
+        }
+        while (col != upC) {
+            System.out.print(matrix[downR][col] + " ");
+            col--;
+        }
+        while (row != upR) {
+            System.out.print(matrix[row][col] + " ");
+            row--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+        int[][] matrix2 = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}};
+        int[][] matrix3 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}};
+        printMatrixCircle(matrix2);
+    }
+}
